20bb9770f0d69bdfb2c20c3de88f0fff28d46b0b
Hussein Salim <husseinsalim419@gmail.com>
2025-06-15 16:48:57 +0300

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -23,7 +23,7 @@ export default function Hero() {
 	// const y = useTransform(scrollY, [0, 300], [0, 100])
 
 	// Mission statement words for animated display
-	const missionWords = ["I", "Love", "Computers", "and", "Coffee"];
+	const missionWords = ["I", "Love", "Computers", "And", "Coffee"];
 
 	// Floating achievements data with improved colors and styling
 	const achievements = [
